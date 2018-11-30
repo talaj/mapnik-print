21ebcbaf6fb755cce6e5c4cc0a49676a3c9c7ecb
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2018-11-30 16:30:26 +0000

    w

--- lib/renderer.hpp
+++ lib/renderer.hpp
@@ -1,27 +1,4 @@
-/*****************************************************************************
- *
- * This file is part of Mapnik (c++ mapping toolkit)
- *
- * Copyright (C) 2015 Artem Pavlenko
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- *****************************************************************************/
-
-#ifndef RENDERER_HPP
-#define RENDERER_HPP
+#pragma once
 
 #include <sstream>
 #include <iomanip>
@@ -57,7 +34,7 @@
     Mapnik must be compiled with Cairo support
 #endif
 
-namespace mapnik_render
+namespace mapnik_print
 {
 
 template <typename ImageType>
@@ -251,5 +228,3 @@ using renderer_type = mapnik::util::variant<renderer<agg_renderer>
                                             >;
 
 }
-
-#endif
